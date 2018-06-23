ad3028f56419fb37f68950bfe2c88220860d0f74
Silky Khatun <silky@Silkys-MacBook-Air.local>
2018-06-23 13:40:39 +0530

    url updated

--- Temperature_django/tempratureapp/urls.py
+++ Temperature_django/tempratureapp/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import  url
 from .import views
 
 urlpatterns = [
-    url(r'^convert/fahrenheit/(?P<temperature>\d+)$', views.fahrenheit, name='fahrenheit'),
-    url(r'^convert/celsius/(?P<temperature>\d+)$', views.celsius, name='celsius'),
-    url(r'^convert/kelvin/(?P<temperature>\d+)$', views.kelvin, name='kelvin'),
-    url(r'^convert/rankine/(?P<temperature>\d+)$', views.rankine, name='rankine'),
+    url(r'^convert/fahrenheit/(?P<temperature>\d+)/$', views.fahrenheit, name='fahrenheit'),
+    url(r'^convert/celsius/(?P<temperature>\d+)/$', views.celsius, name='celsius'),
+    url(r'^convert/kelvin/(?P<temperature>\d+)/$', views.kelvin, name='kelvin'),
+    url(r'^convert/rankine/(?P<temperature>\d+)/$', views.rankine, name='rankine'),
 ]
